10d3f0b55eae6d19345665c66a7c2276902236a5
JohannJohann <johann.oulai@gmail.com>
2021-01-20 04:44:48 +0100

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
+
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new NodePolyfillPlugin()
+    ],
+  target: "node"
+};
